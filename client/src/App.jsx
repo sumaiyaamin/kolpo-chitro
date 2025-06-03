65673af3447ea605a170247d30a7152d708c8c45
sumaiyaamin <sumaiya.prova321@gmail.com>
2025-06-03 23:39:33 +0600

    tailwind and routing setup and pages mounted at app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Home from './pages/Home'
+import BuyCredit from './pages/BuyCredit'
+import Result from './pages/Result'
 
 const App = () => {
   return (
     <div>
-      
+      <Home />
+      <BuyCredit />
+      <Result />
     </div>
   )
 }
